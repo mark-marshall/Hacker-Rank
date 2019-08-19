5566e79d86c8d19e0da629af6427255e1256b7c2
mark-marshall <mark.marshallgp@gmail.com>
2019-08-19 19:37:08 +0100

    ransom note hash_tables

---
+++ Hash_Tables/ransom_note.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/ctci-ransom-note/problem
+def checkMagazine(magazine, note):
+    hash_table = {}
+    for mag_word in magazine:
+        if mag_word in hash_table:
+            hash_table[mag_word] += 1
+        else:
+            hash_table[mag_word] = 1
+    for note_word in note:
+        if note_word in hash_table and hash_table[note_word]:
+            hash_table[note_word] -= 1
+        else:
+            return "No"
+    return "Yes"
+    
