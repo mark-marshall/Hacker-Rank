aecb125abf99da6f7d257bb4b6cb4a67b636b5fc
mark-marshall <mark.marshallgp@gmail.com>
2019-10-01 13:47:18 +0100

    lisas_workbook in py,js

---
+++ Implementation/lisas_workbook.py
@@ -0,0 +1,46 @@
+# https://www.hackerrank.com/challenges/lisa-workbook/problem
+
+# Python
+def workbook(n, k, arr):
+  num_special_problems = 0
+  cur_page = 0
+  problem_on_page = 0;
+  for chapter in arr:
+    cur_page += 1
+    problem_on_page = 0
+    for i in range(chapter):
+      problem = i + 1
+      if problem_on_page + 1 > k:
+        cur_page += 1
+        problem_on_page = 1;
+      else:
+        problem_on_page += 1
+      if problem == cur_page:
+        num_special_problems += 1
+  return num_special_problems
+  
+# JavaScript
+const workbook = (n,k,arr) => {
+  let num_special_problems = 0
+  let cur_page = 0
+  let problems_on_page = 0
+  arr.forEach(chapter => {
+    cur_page += 1
+    let problem_on_page = 0
+    let i;
+    for (i=0;i<chapter;i++){
+      const problem = i + 1
+      if(problem_on_page + 1 > k){
+        cur_page += 1
+        problem_on_page = 1;
+      }
+      else {
+        problem_on_page += 1
+      }
+      if(problem === cur_page){
+        num_special_problems += 1
+      }
+    }
+  })
+  return num_special_problems
+}
