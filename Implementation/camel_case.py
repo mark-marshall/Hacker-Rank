cf28622444c66124318bbf51276c892d0d0326f2
mark-marshall <mark.marshallgp@gmail.com>
2019-10-09 17:01:14 +0100

    Removed semi colon from camelCase

--- Implementation/camel_case.py
+++ Implementation/camel_case.py
@@ -1,8 +1,7 @@
 # https://www.hackerrank.com/challenges/camelcase/problem
 def camelcase(s):
-  count = 1;
+  count = 1
   for char in s:
     if char == char.upper():
       count += 1
   return count
-  
