afb3fc493bd6371f487eb3aa1ff028ce453b0424
mark-marshall <mark.marshallgp@gmail.com>
2019-10-17 12:32:18 +0100

    Changed PDF viewer to use string object

--- Implementation/designer_pdf_viewer.py
+++ Implementation/designer_pdf_viewer.py
@@ -1,9 +1,7 @@
 # https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 def designerPdfViewer(h, word):
   max_h = 0
-  chars = 'abcdefghijklmnopqrstuvwxyz'
   for idx,height in enumerate(h):
-    if (chars[idx] in word) and (height > max_h):
+    if (string.ascii_lowercase[idx] in word) and (height > max_h):
       max_h = height
   return max_h * len(word)
-  
